59b88a1a2ae00f170c6b2dabee45b0fd6aed3175
Iori <Iori@DESKTOP-IQIH30C>
2020-09-14 14:43:22 +0800

    添加项目文件。

---
+++ MultiTenantDemo/Model/TenantInfoMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MultiTenantDemo.Model
+{
+    public class TenantInfoMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public TenantInfoMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var tenantInfo = context.RequestServices.GetRequiredService<TenantInfo>();
+            var tenantName = context.Request.Headers["Tenant"];
+
+            if(string.IsNullOrEmpty(tenantName))
+            {
+                tenantName = "default";
+            }
+
+            tenantInfo.Name = tenantName;
+
+            await _next(context);
+        }
+    }
+}
